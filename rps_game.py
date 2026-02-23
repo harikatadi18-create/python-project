9dbac04476cb65006e211bd00a7f3a176fbc86bc
harikatadi18-create <harikatadi18@gmail.com>
2026-02-23 20:44:06 +0530

    updated RPS to Best of 3 version

--- rps_game.py
+++ rps_game.py
@@ -1,26 +1,44 @@
 import random
-print("🎮 Rock papermScissors Game")
-choices=["rock", "paper", "Scissors"]
-computer=random.choice(choices)
-user=input("enter rock,paper,or scissors: ").lower()
-print("computer chose:",computer)
-if user==computer:
-    print("it's tie!")
-elif user=="rock":
-    if computer=="Scissors":
-        print("you win!")
-    else:
-        print("you lose!")
-elif user=="paper":
-    if computer=="rock":
-        print("you win!")
-    else:
-        print("you lose!")
-elif user=="scissors":
-    if computer=="paper":
-        print("you win!")
-    else:
-        print("you lose!")
-else:
-    print("Invlaid input")
+
+print("🎮 Rock Paper Scissors - Best of 3")
+
+choices = ["rock", "paper", "scissors"]
+
+user_score = 0
+computer_score = 0
+
+for round in range(1, 4):
+    print(f"\nRound {round}")
     
+    computer = random.choice(choices)
+    user = input("Enter rock, paper, or scissors: ").lower()
+
+    print("Computer chose:", computer)
+
+    if user == computer:
+        print("It's a tie!")
+    elif user == "rock" and computer == "scissors":
+        print("You win this round! 🎉")
+        user_score += 1
+    elif user == "paper" and computer == "rock":
+        print("You win this round! 🎉")
+        user_score += 1
+    elif user == "scissors" and computer == "paper":
+        print("You win this round! 🎉")
+        user_score += 1
+    elif user in choices:
+        print("Computer wins this round!")
+        computer_score += 1
+    else:
+        print("Invalid input!")
+
+print("\nFinal Score:")
+print("You:", user_score)
+print("Computer:", computer_score)
+
+if user_score > computer_score:
+    print("🏆 You are the overall winner!")
+elif computer_score > user_score:
+    print("💻 Computer is the overall winner!")
+else:
+    print("🤝 It's a tie match!")
